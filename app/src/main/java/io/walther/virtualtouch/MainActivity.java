0d3368aa2d058c223b212b236de5623e6aba3227
Enrique Sterling <enristerling@gmail.com>
2016-04-04 23:01:26 -0500

    Improved home page UI

--- app/src/main/java/io/walther/virtualtouch/MainActivity.java
+++ app/src/main/java/io/walther/virtualtouch/MainActivity.java
@@ -22,15 +22,6 @@ public class MainActivity extends AppCompatActivity {
 
         TextView appTitleTextView = (TextView) findViewById(R.id.appTitle);
         appTitleTextView.setText(R.string.app_name);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
